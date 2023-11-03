1a4745b786cb405cf918c274e7e07b00c8d016e3
radentv <96883846+radentv@users.noreply.github.com>
2023-11-03 00:14:44 -0300

    Add files via upload

---
+++ wafx_res/generated/wafxCommonPhpPage.php
@@ -0,0 +1,13 @@
+<?php
+$global_webacappella_auth_chain=[
+'auth_chain'=>[
+'{1e607477-36ec-4fff-a3ce-b88497e4af6a}'=>[
+'auth'=>['{1e607477-36ec-4fff-a3ce-b88497e4af6a}'],'hash'=>'8de1cd276a57dde844b3213b41403628564d9a01','login'=>'juniorbher','uuid'=>'{1e607477-36ec-4fff-a3ce-b88497e4af6a}'
+]
+],'unid'=>'b9a8d4b7_65c3_4514_aa0f_258675126d3b'
+];
+$global_webacappella_messages=[
+'fr'=>[
+'webmessage:cookiebanner:i understand'=>'J\'accepte','webmessage:cookiebanner:learn more'=>'Plus d\'information','webmessage:cookiebanner:this website use cookie to ensure good experience on our website'=>'Ce site utilise des cookies pour vous assurer la meilleure expérience de navigation','webmessage:dialog:close'=>'Fermer','webmessage:formulary:form successfully sent !'=>'Formulaire envoyé avec succès !','webmessage:freeversion:created by webacappella'=>'Créez votre site maintenant avec WebAcappella','webmessage:freeversion:new discover sshdesk software'=>'Nouveau ! : Découvrez SshDesk ,réservé aux pros du Web pour gérer leurs boutiques et leurs applications Web','webmessage:freeversion:powered by webacappella, create your site now'=>'Ce site à été conçu avec le logiciel de création de sites Internet WebAcappella, Créez votre site maintenant','webmessage:page:untitled page'=>'Page sans titre','webmessage:restrictarea:button label'=>'Envoyer','webmessage:restrictarea:error authentication failed'=>'Erreur d\'authentification, verifiez l\'identifiant et le mot de passe','webmessage:restrictarea:feature not avalaible'=>'Fonctionnalité non disponible avec cette licence','webmessage:restrictarea:login placeholder'=>'Login','webmessage:restrictarea:password placeholder'=>'Senha','webmessage:restrictarea:welcome message'=>'Obrigado Por Se Autenticar para Acessar A Pagina ','webmessage:search:no result found !'=>'Pas de résultat trouvé !','webmessage:search:please fill criterias'=>'Rechercher sur le site','webmessage:search:search placeholder'=>'Rechercher'
+]
+];
---
+++ wafx_res/generated/wafxSearchFullText_fr.js
@@ -0,0 +1,21 @@
+ { 
+"map_targets":[
+ {  "title":"RADEN TV" , "display_url":"index.html" , "url":"index.html" } 
+],
+"map_words":[""," iptv"," listas gratis"," tvbox","aqui","e","inicio","iptv","raden","radentv","sem","travamentos","tv"]
+,
+"index_words":[ [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+, [0 ] 
+]
+ } 
