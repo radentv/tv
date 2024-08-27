29f28e06a00ec4dab93b00601480d438427e7457
radentv <96883846+radentv@users.noreply.github.com>
2024-08-26 23:09:22 -0300

    Add files via upload

---
+++ ap/dns.php
@@ -0,0 +1,1 @@
+{"ftg":true,"status":true,"su":"http://play.stmlist.vip,http://cdnfast.online,http://shiuk.re:80,http://zplay.dnsbr.shop:80,http://megaultra.xyz:80,http://dnsvia.xctv.app,http://ativabox.top,http://netturbo.aovivo.xyz,http://onyxhd.top,http://webtvhp.online:80","sc":"","ndd":""}
